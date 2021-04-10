e9faf6aafab142bd0e25974b7c0e7422ef90e864
JOSEPH <josepholukunle1107@gmail.com>
2021-04-10 14:44:33 +0100

    Loop note

--- index.py
+++ index.py
@@ -1,1 +1,16 @@
-print("This is a python file.")
+# Loop
+# alist = [1, 2, 3, 4, 5, 6]
+
+# for item in alist:
+#     print(item)
+
+# for x in range(5):
+#     if(not(x == 0)):
+#         print(x)
+
+# While loop
+count = 5
+while count > 0:
+    print(count)
+    count -= 1
+ 
