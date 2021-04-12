bf8dfc2e5b2ec2ce886c8466f09f2dd4ac873c2d
JOSEPH <josepholukunle1107@gmail.com>
2021-04-12 10:53:59 +0100

    Added an auth.py file for dictionary class

---
+++ auth.py
@@ -0,0 +1,23 @@
+# register
+# login
+
+# Dictionary
+aSampleList = [1, 2, 3]
+
+# Method 1
+dictionary1 = {
+    "key1": "value1",
+    "key2": "value2",
+    "key3": "value3"
+}
+
+# Method2
+dictionary2 = {}
+
+dictionary2["key4"] = "value4"
+dictionary2["key5"] = "value5"
+dictionary2["key6"] = "value6"
+
+print(dictionary1)
+print(aSampleList)
+print(dictionary2)
